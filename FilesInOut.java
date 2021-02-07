58170db198a92f77ebf7d5cd9e0e4fef46ef7e03
Saemundur Haraldsson <67325496+saemundur-haraldsson@users.noreply.github.com>
2021-02-07 18:57:35 +0000

    Add files via upload

---
+++ FilesInOut.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.lang.Number;
+
+/**
+ * 
+ * CSCU9T4 Java strings and files exercise.
+ *
+ */
+public class FilesInOut {
+
+    public static void main(String[] args) {
+        // Replace this with statements to set the file name (input) and file name (output).
+        // Initially it will be easier to hardcode suitable file names.
+
+        // Set up a new Scanner to read the input file.
+        // Processing line by line would be sensible here.
+        // Initially, echo the text to System.out to check you are reading correctly.
+        // Then add code to modify the text to the output format.
+
+        // Set up a new PrintWriter to write the output file.
+        // Add suitable code into the above processing (because you need to do this line by line also.
+        // That is, read a line, write a line, loop.
+
+        // Finally, add code to read the filenames as arguments from the command line.
+
+        System.out.println("You need to add your own code to do anything");
+
+    } // main
+
+} // FilesInOut
